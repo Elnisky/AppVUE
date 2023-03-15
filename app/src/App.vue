76a99e7d7f9ea559b5576bf6162f22f4af53acb6
elnisky <leandro_elnisky@hotmail.com>
2023-03-15 19:11:32 -0300

    start-03

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,63 +1,19 @@
 <template>
-  <h1>{{titulo}}</h1>
-  <fieldset>
-    <legend>Itens</legend>
-    <ul>
-      <li v-for="item in itens" :key = item.id>
-        <p v-if="!item.editando">
-          {{ item.id }} - {{ item.nome }}
-            <button v-if="item.finalizado" @click="excluir(item)">Excluir</button>
-            <button v-else @click="alterar(item)">Editar</button>
-        </p>
-        <div v-else>
-          <input v-model="item.nome">  
-          <button @click="confirmar(item)">Confirmar</button>
-        </div>
-      </li>
-    </ul>
-  </fieldset>
+  <div>
+    <nav class="navbar navbar-dark bg-primary justify-content-between">
+      <div>
+        <ul>
+          <li>
+            <router-link to="/">Novo</router-link>
+          </li>
+          <li>
+            <router-link to="/view">Visualizar</router-link>
+          </li>
+          <li>
+            <router-link to="/edit">Editar</router-link>
+          </li>          
+        </ul>
+      </div>
+    </nav>
+  </div>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  data(){
-    return{
-      titulo: "Atividades",
-      itens: [
-        {id: 1, nome: "Item 001", finalizado: false, editando: false},
-        {id: 2, nome: "Item 002", finalizado: true, editando: false},
-        {id: 3, nome: "Item 003", finalizado: true, editando: false}
-      ]
-    }
-  },
-  methods:{
-    excluir(item){
-      if(confirm('Tem certeza que deseja excluir o item ' + item.nome + '?')){
-        this.itens = this.itens.filter(x => x.id != item.id);
-      }
-    },
-    alterar(item){
-      let registro = this.itens.find((x) => {
-        return x.id === item.id;
-      });
-      registro.editando = true;
-    },
-    confirmar(item){
-      alert(item.nome);
-    }
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ app/src/router/index.js
@@ -0,0 +1,30 @@
+import { createWebHistory, createRouter } from "vue-router";
+import CreateComponent from "@components/CreateComponent.vue";
+import ListComponent from "@components/ListComponent.vue";
+import EditComponent from "@components/EditComponent.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "home",
+        component: CreateComponent
+    },
+    {
+        path: "/view",
+        name: "view",
+        component: ListComponent
+    },
+    {
+        path: "/edit",
+        name: "edit",
+        component: EditComponent
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router;
+
