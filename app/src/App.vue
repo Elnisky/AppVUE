749d2044bb86dcd6254b4d5b0d2978fffa0ad8c5
elnisky <leandro_elnisky@hotmail.com>
2023-03-14 21:02:45 -0300

    start-02

--- app/src/App.vue
+++ app/src/App.vue
@@ -4,11 +4,15 @@
     <legend>Itens</legend>
     <ul>
       <li v-for="item in itens" :key = item.id>
-        <p>
+        <p v-if="!item.editando">
           {{ item.id }} - {{ item.nome }}
-            <button v-if="item.finalizado">Excluir</button>
-            <button v-else>Editar</button>
+            <button v-if="item.finalizado" @click="excluir(item)">Excluir</button>
+            <button v-else @click="alterar(item)">Editar</button>
         </p>
+        <div v-else>
+          <input v-model="item.nome">  
+          <button @click="confirmar(item)">Confirmar</button>
+        </div>
       </li>
     </ul>
   </fieldset>
@@ -22,10 +26,26 @@ export default {
     return{
       titulo: "Atividades",
       itens: [
-        {id: 1, nome: "Item 001", finalizado: false},
-        {id: 2, nome: "Item 002", finalizado: true},
-        {id: 3, nome: "Item 003", finalizado: true}
+        {id: 1, nome: "Item 001", finalizado: false, editando: false},
+        {id: 2, nome: "Item 002", finalizado: true, editando: false},
+        {id: 3, nome: "Item 003", finalizado: true, editando: false}
       ]
+    }
+  },
+  methods:{
+    excluir(item){
+      if(confirm('Tem certeza que deseja excluir o item ' + item.nome + '?')){
+        this.itens = this.itens.filter(x => x.id != item.id);
+      }
+    },
+    alterar(item){
+      let registro = this.itens.find((x) => {
+        return x.id === item.id;
+      });
+      registro.editando = true;
+    },
+    confirmar(item){
+      alert(item.nome);
     }
   }
 }
